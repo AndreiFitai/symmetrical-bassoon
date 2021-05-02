bb42b05c05fedbae6035df70d52ee2f9aa9e29ef
Andrei Fitai <andrei.fitai@frontiercargroup.com>
2021-05-02 22:32:08 +0200

    initial projects setup

---
+++ subtitle-service/test/index.spec.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai'
+
+const expect = chai.expect
+
+describe('Example test', () => {
+	it('should run a test succesfuly', () => {
+		expect(1).to.equal(1)
+	})
+})
---
+++ translation-service/src/index.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv'
+import express, { Application, Request, Response } from 'express'
+
+config()
+
+const { PORT } = process.env
+
+const app: Application = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello world!')
+})
+
+app.listen(PORT, () => {
+	console.log(`translation-service server started at http://localhost:${PORT}`)
+})
